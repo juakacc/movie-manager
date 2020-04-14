6c74a10289af74f035889531012d17cf00b2c37e
Juaka <juakacc@gmail.com>
2020-04-14 11:49:49 -0300

    Listagem de filmes, com scroll infinito, ordenando e consumindo a API

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { debounceTime } from "rxjs/operators";
 import { FilmesService } from "src/app/core/filmes.service";
 import { Filme } from "src/app/shared/models/filme";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { ConfigParams } from "src/app/shared/models/config-params";
 
 @Component({
   selector: "dio-listagem-filmes",
@@ -8,15 +11,73 @@ import { Filme } from "src/app/shared/models/filme";
   styleUrls: ["./listagem-filmes.component.scss"],
 })
 export class ListagemFilmesComponent implements OnInit {
-  filmes: Filme[];
+  readonly semFoto =
+    "https://www.buritama.sp.leg.br/imagens/parlamentares-2013-2016/sem-foto.jpg";
+  filmes: Filme[] = [];
 
-  constructor(private filmesService: FilmesService) {}
+  config: ConfigParams = {
+    pagina: 0,
+    limite: 4,
+  };
+
+  filtrosListagem: FormGroup;
+  generos: String[];
+
+  constructor(private filmesService: FilmesService, private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.filmesService.listar().subscribe((filmes: Filme[]) => {
-      this.filmes = filmes;
+    this.filtrosListagem = this.fb.group({
+      texto: [""],
+      genero: [""],
+    });
+
+    this.filtrosListagem
+      .get("texto")
+      .valueChanges.pipe(debounceTime(400))
+      .subscribe((val: string) => {
+        this.config.pesquisa = val;
+        this.resetarConsulta();
+      });
+
+    this.filtrosListagem.get("genero").valueChanges.subscribe((val: string) => {
+      this.config.campo = {
+        tipo: "genero",
+        valor: val,
+      };
+      this.resetarConsulta();
+    });
+
+    this.generos = [
+      "Ação",
+      "Romance",
+      "Comédia",
+      "Aventura",
+      "Terror",
+      "Ficção cientifíca",
+      "Drama",
+    ];
+    this.getFilmes();
+  }
+
+  onScroll(): void {
+    this.getFilmes();
+  }
+
+  open(id: number): void {
+    console.log(id);
+  }
+
+  private getFilmes(): void {
+    this.config.pagina++;
+
+    this.filmesService.listar(this.config).subscribe((filmes: Filme[]) => {
+      this.filmes.push(...filmes);
     });
   }
 
-  open() {}
+  private resetarConsulta(): void {
+    this.config.pagina = 0;
+    this.filmes = [];
+    this.getFilmes();
+  }
 }
